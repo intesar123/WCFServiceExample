a5bab5fb651ac38d65deef055724a5c4b6f1d12b
Intesar Alam <alam.kir@gmail.com>
2017-10-23 21:54:01 +0530

    23102017

---
+++ WebApplication1/WebApplication1/Index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication1.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <asp:Button runat="server" ID="btnShowMessage" Text="Get Message" OnClick="btnShowMessage_Click" /><br />
+       WCF Message: <asp:Label runat="server" ID="msg"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        ServiceReference1.Service1Client proxy;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                proxy = new ServiceReference1.Service1Client();
+                msg.Text = proxy.GetData(11);   
+            }
+        }
+
+        protected void btnShowMessage_Click(object sender, EventArgs e)
+        {
+            proxy = new ServiceReference1.Service1Client();
+            msg.Text = proxy.GetData(11);
+        }
+    }
+}

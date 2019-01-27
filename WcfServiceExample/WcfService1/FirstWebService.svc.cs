63325bc9fb52bb47abaa99da9d48aa93166ff2af
Intesar Alam <alam.kir@gmail.com>
2019-01-27 23:58:07 +0530

    27012018

--- WcfServiceExample/WcfService1/FirstWebService.svc.cs
+++ WcfServiceExample/WcfService1/FirstWebService.svc.cs
@@ -9,6 +9,7 @@ namespace WcfService1
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FirstWebService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select FirstWebService.svc or FirstWebService.svc.cs at the Solution Explorer and start debugging.
+    //Note: Go to C:\ProgramData\Microsoft\Crypto\RSA give read/write permission of folder 'MachineKeys' to users
     public class FirstWebService : IFirstWebService
     {
         public string Message()
